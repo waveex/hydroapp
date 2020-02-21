639e79644efb46b393666d89ed40df7303b77f2a
Michal <michal.jablonski097@gmail.com>
2020-02-21 14:51:46 +0100

    feat: main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,60 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+const count = document.querySelector('.glass__count');
+const add = document.querySelector('.button__add--js') ;
+const remove = document.querySelector('.button__remove--js');
+const scoreyesterday = document.querySelector('.table__a--js');
+const scoretoday = document.querySelector('.table__b--js');
+const dayb = document.querySelector('.table__yesterday--js');
+const day = document.querySelector('.table__today--js');
+const key = new Date().toISOString().slice(0, 10)
 
-// place your code below
-
-
- const count = document.querySelector('.glass__count')
  
- const add = document.querySelector('.button__add--js')
-
-/* const remove = document.querySelector('.button__remove--js')
-add.addEventListener ('click', (e) => {
-add.innerHTML.toggle('+1') 
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+  count.innerHTML = "0"
 }
+else {
+  count.localStorage = localStorage.getItem(key);
+  count.innerHTML = localStorage.getItem(key);
+  scoretoday.innerHTML = localStorage.getItem(key)
+} 
+
+   
+ 
+
+
+
+
+
+add.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+  count.innerHTML = localStorage.getItem(key);
+  scoretoday.innerHTML = localStorage.getItem(key);
+})
+
+
+remove.addEventListener('click', (e) => {
+  e.preventDefault();
+  const value = parseInt(count.innerHTML);
+  if (value > 0) {
+    localStorage.setItem(key, parseInt(localStorage.getItem(key)) - 1)
+    count.innerHTML = localStorage.getItem(key);
+  }
+  scoretoday.innerHTML = localStorage.getItem(key);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
